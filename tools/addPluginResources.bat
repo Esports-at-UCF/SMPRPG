d4d6bfb108fb5d804545fc319d25ae33f8a96db8
CheezedFish <watdou606@gmail.com>
2026-05-02 21:51:38 -0400

    Tools: Add rudimentary helper tool for staging commits

---
+++ tools/addPluginResources.bat
@@ -0,0 +1,4 @@
+git add ../run/plugins/CraftEngine/resources -f
+git add ../run/plugins/BetterModel/armors -f
+git add ../run/plugins/BetterModel/models -f
+git add ../run/plugins/BetterModel/players -f
