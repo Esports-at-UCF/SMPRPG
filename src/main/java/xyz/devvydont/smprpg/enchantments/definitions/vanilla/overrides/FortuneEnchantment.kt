80e0f8baf06ad06e30edefa54505e0392a74d803
CheezedFish <watdou606@gmail.com>
2026-06-15 00:05:46 -0400

    Enchantments: Add recipes for fortune

--- src/main/java/xyz/devvydont/smprpg/enchantments/definitions/vanilla/overrides/FortuneEnchantment.kt
+++ src/main/java/xyz/devvydont/smprpg/enchantments/definitions/vanilla/overrides/FortuneEnchantment.kt
@@ -6,6 +6,7 @@ import io.papermc.paper.registry.tag.TagKey
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Color
+import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.EquipmentSlotGroup
 import org.bukkit.inventory.ItemType
@@ -13,6 +14,8 @@ import xyz.devvydont.smprpg.attribute.AttributeWrapper
 import xyz.devvydont.smprpg.enchantments.EnchantmentRarity
 import xyz.devvydont.smprpg.enchantments.base.AttributeEnchantment
 import xyz.devvydont.smprpg.enchantments.definitions.vanilla.VanillaEnchantment
+import xyz.devvydont.smprpg.enchantments.recipe.EnchantmentRecipe
+import xyz.devvydont.smprpg.items.CustomItemType
 import xyz.devvydont.smprpg.items.attribute.AttributeEntry
 import xyz.devvydont.smprpg.items.attribute.AttributeModifierType
 import xyz.devvydont.smprpg.util.formatting.ComponentUtils
@@ -42,6 +45,92 @@ class FortuneEnchantment(key: TypedKey<Enchantment>) : VanillaEnchantment(key), 
         return mutableListOf(
             AttributeEntry.additive(AttributeWrapper.MINING_FORTUNE, getFortune(level).toDouble())
         )
+    }
+
+    override fun getRecipe(level: Int): EnchantmentRecipe? {
+        when (level) {
+            1 -> {
+                val gold = getIngredientStack(Material.GOLD_INGOT, 8)
+                val diamond = getIngredientStack(Material.DIAMOND, 4)
+                val lapis = getIngredientStack(Material.LAPIS_LAZULI, 8)
+                return EnchantmentRecipe(getRecipeKey(level), 0, gold, diamond, lapis)
+            }
+
+            2 -> {
+                val gold = getIngredientStack(Material.GOLD_INGOT, 16)
+                val diamond = getIngredientStack(Material.DIAMOND, 8)
+                val lapis = getIngredientStack(Material.LAPIS_LAZULI, 16)
+                return EnchantmentRecipe(getRecipeKey(level), 5, gold, diamond, lapis)
+            }
+
+            3 -> {
+                val gold = getIngredientStack(Material.GOLD_INGOT, 32)
+                val diamond = getIngredientStack(Material.DIAMOND, 16)
+                val lapis = getIngredientStack(Material.LAPIS_LAZULI, 32)
+                return EnchantmentRecipe(getRecipeKey(level), 10, gold, diamond, lapis)
+            }
+
+            4 -> {
+                val gold = getIngredientStack(Material.GOLD_INGOT, 64)
+                val diamond = getIngredientStack(Material.DIAMOND, 32)
+                val mithril = getIngredientStack(CustomItemType.MITHRIL_INGOT, 64)
+                val lapis = getIngredientStack(Material.LAPIS_LAZULI, 64)
+                return EnchantmentRecipe(getRecipeKey(level), 20, gold, diamond, mithril, lapis)
+            }
+
+            5 -> {
+                val gold = getIngredientStack(Material.GOLD_BLOCK, 15)
+                val diamond = getIngredientStack(Material.DIAMOND, 64)
+                val mithril = getIngredientStack(CustomItemType.MITHRIL_BLOCK, 15)
+                val lapis = getIngredientStack(Material.LAPIS_BLOCK, 16)
+                return EnchantmentRecipe(getRecipeKey(level), 30, gold, diamond, mithril, lapis)
+            }
+
+            6 -> {
+                val gold = getIngredientStack(Material.GOLD_BLOCK, 30)
+                val diamond = getIngredientStack(Material.DIAMOND_BLOCK, 15)
+                val mithril = getIngredientStack(CustomItemType.MITHRIL_BLOCK, 30)
+                val lapis = getIngredientStack(Material.LAPIS_BLOCK, 32)
+                return EnchantmentRecipe(getRecipeKey(level), 40, gold, diamond, mithril, lapis)
+            }
+
+            7 -> {
+                val gold = getIngredientStack(Material.GOLD_BLOCK, 60)
+                val diamond = getIngredientStack(Material.DIAMOND_BLOCK, 30)
+                val mithril = getIngredientStack(CustomItemType.MITHRIL_BLOCK, 60)
+                val platinum = getIngredientStack(CustomItemType.PLATINUM_BLOCK, 4)
+                val lapis = getIngredientStack(Material.LAPIS_BLOCK, 64)
+                return EnchantmentRecipe(getRecipeKey(level), 50, gold, diamond, mithril, platinum, lapis)
+            }
+
+            8 -> {
+                val gold = getIngredientStack(CustomItemType.ENCHANTED_GOLD, 14)
+                val diamond = getIngredientStack(Material.DIAMOND_BLOCK, 60)
+                val mithril = getIngredientStack(CustomItemType.ENCHANTED_MITHRIL, 14)
+                val platinum = getIngredientStack(CustomItemType.PLATINUM_BLOCK, 8)
+                val lapis = getIngredientStack(CustomItemType.ENCHANTED_LAPIS, 16)
+                return EnchantmentRecipe(getRecipeKey(level), 60, gold, diamond, mithril, platinum, lapis)
+            }
+
+            9 -> {
+                val gold = getIngredientStack(CustomItemType.ENCHANTED_GOLD, 28)
+                val diamond = getIngredientStack(CustomItemType.ENCHANTED_DIAMOND, 14)
+                val mithril = getIngredientStack(CustomItemType.ENCHANTED_MITHRIL, 28)
+                val platinum = getIngredientStack(CustomItemType.PLATINUM_BLOCK, 16)
+                val lapis = getIngredientStack(CustomItemType.ENCHANTED_LAPIS, 32)
+                return EnchantmentRecipe(getRecipeKey(level), 70, gold, diamond, mithril, platinum, lapis)
+            }
+
+            10 -> {
+                val gold = getIngredientStack(CustomItemType.ENCHANTED_GOLD, 56)
+                val diamond = getIngredientStack(CustomItemType.ENCHANTED_DIAMOND, 28)
+                val mithril = getIngredientStack(CustomItemType.ENCHANTED_MITHRIL, 56)
+                val platinum = getIngredientStack(CustomItemType.PLATINUM_BLOCK, 32)
+                val lapis = getIngredientStack(CustomItemType.ENCHANTED_LAPIS, 64)
+                return EnchantmentRecipe(getRecipeKey(level), 80, gold, diamond, mithril, platinum, lapis)
+            }
+            else -> return null
+        }
     }
 
     companion object {
