bf05b8a4800f9a33ccbdc0da6da0511c53f6c625
dev <49467408+DevvyDont@users.noreply.github.com>
2026-06-22 00:17:12 -0400

    inventory: fix desync issues when clicking 4x4 shortcut buttons

--- src/main/java/xyz/devvydont/smprpg/listeners/entity/PlayerInventoryButtonsListener.kt
+++ src/main/java/xyz/devvydont/smprpg/listeners/entity/PlayerInventoryButtonsListener.kt
@@ -19,26 +19,34 @@ class PlayerInventoryButtonsListener: ToggleableListener() {
     @EventHandler
     private fun onPlayerClickCraftButton(event: InventoryClickEvent) {
         val clickedInventory = event.clickedInventory
-        if (clickedInventory == null) return
+            ?: return
 
         if (clickedInventory.type == InventoryType.CRAFTING) {
             val player = event.whoClicked as Player
-            when (event.slot) {
-                1 -> {
-                    MenuType.CRAFTING.builder()
-                        .location(player.location)
-                        .checkReachable(false)
-                        .title(Component.text("Crafting"))
-                        .build(player)
-                        .open()
+            player.closeInventory()
+            Bukkit.getScheduler().runTaskLater(SMPRPG.plugin, Runnable {
+                when (event.slot) {
+                    1 -> {
+                        Bukkit.getScheduler().runTaskLater(SMPRPG.plugin, Runnable {
+                            MenuType.CRAFTING.builder()
+                                .location(player.location)
+                                .checkReachable(false)
+                                .title(Component.text("Crafting"))
+                                .build(player)
+                                .open()
+                        }, 1L)
+                    }
+                    2 ->  {
+                        Bukkit.getScheduler().runTaskLater(SMPRPG.plugin, Runnable {
+                            InterfaceWardrobe(null, player, player).openMenu()
+                        }, 1L)
+                    }
+                    3 -> InterfaceStats(player, player, null).openMenu()
+                    4 -> MenuStatsRoot(player, player, null).openMenu()
+                    -999 -> return@Runnable
+                    else -> event.isCancelled = true
                 }
-                2 -> InterfaceWardrobe(null, player, player).openMenu()
-                3 -> InterfaceStats(player, player, null).openMenu()
-                4 -> MenuStatsRoot(player, player, null).openMenu()
-                -999 -> return
-                else -> event.isCancelled = true
-            }
-            Bukkit.getScheduler().runTaskLater(SMPRPG.plugin, Runnable { player.updateInventory() }, 1L)
+            }, 1L)
         }
     }
 }
